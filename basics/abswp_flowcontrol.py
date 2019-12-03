c6ca70e4a4474f4564665a8c3a9319c16a8afb6c
markillob <cummy9@hotmail.com>
2019-12-02 23:21:20 -0800

    while, elif, if input, falsey

---
+++ basics/abswp_flowcontrol.py
@@ -0,0 +1,60 @@
+def if_statement ( name : str ) -> str :
+    first_name = 'Alice'
+    if name == first_name :
+        print("%s its is my name" % first_name)
+    else:    
+        print ('Hi %s' % name)
+    return name
+
+#if_statement("marco")
+
+def elif_statement(name: str, age : int) -> str :
+    local_age = 3000
+    if name == 'Alice':
+        print('alice')
+    elif age < 12 :
+        print ('local age %d' % local_age)
+    elif age > 200 :
+        print ('Alice is not an undead')
+    else:
+        print('Hola Mundo')
+    return 'Hola'
+
+#elif_statement('Marco',6 )
+
+def get_name(name: str) -> str :
+    print(name)
+    #truty falsey value
+    #empty strins is falsey or 0 value
+    name_local = input()
+    if name_local:
+        print("Thanks for entering a name %s" % name_local)
+    else:
+        print("Try again")
+        return 
+    return print("Your name is %s" % name_local)
+
+#get_name('Enter your name')
+
+def while_statement( age : int) -> int:
+    born_age = 0
+    while born_age <= age:
+        print("You age like %d" %born_age)
+        born_age+=1
+    return age
+
+#while_statement(10)
+def guess_your_name_3_times( name : str) -> str :
+    count = 0
+    while True:
+        print ("What is your name")
+        local_name = input()
+        if name == local_name or count >= 3:
+            break
+        else:
+            count+=1
+            print("Keep trying")
+    return
+#break statement jump out of the lopp
+#continue reevaluates the loop condition
+#guess_your_name_3_times('Marco')
